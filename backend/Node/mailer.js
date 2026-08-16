b262d0e448d4f420aa03a1d0f23866eba4da242b
vijaysinh09 <kamblevijaysinh09@gmail.com>
2026-08-16 09:44:55 +0530

    Change default SMTP port to 2525 to bypass Render firewall

--- backend/Node/mailer.js
+++ backend/Node/mailer.js
@@ -2,8 +2,8 @@ const nodemailer=require("nodemailer");
 
 const transporter=nodemailer.createTransport({
     host: process.env.BREVO_HOST || "smtp-relay.brevo.com",
-    port: process.env.BREVO_PORT || 587,
-    secure: false, // Use false for port 587
+    port: process.env.BREVO_PORT || 2525,
+    secure: false, // Use false for port 2525 or 587
     auth: {
         user: process.env.BREVO_USER || "",
         pass: process.env.BREVO_API_KEY || ""
